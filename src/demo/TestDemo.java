13b5f7c3527fdeeb3d9d5229d239a12c12fde3cf
Kazim Daqiqi <Kazim Daqiqi@Mohamma.mynetworksettings.com>
2024-01-07 13:11:16 -0500

    Merge branch 'master' of https://github.com/kazimdaqiqi/Project1.git

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -1,10 +1,12 @@
 package demo;
 
 public class TestDemo {
-	public static void main(String [] args) {
-		System.out.println("Day 2 GitHub.........");
+	public static void main(String []args) {
+		System.out.println("Day 2 GitHub............");
+		System.out.println("Exercise 1............");
 	}
-	public static void substract(int a, int b){
-		System.out.println(a-b);
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
