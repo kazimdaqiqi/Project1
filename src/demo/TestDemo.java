38aa34e77abe96ed5475823764cd7e1c25935f75
Kazim Daqiqi <Kazim Daqiqi@Mohamma.mynetworksettings.com>
2024-01-07 14:40:16 -0500

    Added comment

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -1,6 +1,7 @@
 package demo;
 
 public class TestDemo {
+	// Add your code - King Kazim 
 	public static void main(String []args) {
 		System.out.println("Day 2 GitHub............");
 		System.out.println("Exercise 1............");
