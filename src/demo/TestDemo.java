41a2b15891d41f54994cb63fe2e02fec67ed0909
kazimdaqiqi <155591206+kazimdaqiqi@users.noreply.github.com>
2024-01-07 12:55:49 -0500

    Update TestDemo.java
    Subtract

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -4,5 +4,7 @@ public class TestDemo {
 	public static void main(String [] args) {
 		System.out.println("King Kazim.........");
 	}
+	public static void substract(int a, int b){
+		System.out.println(a-b);
 
 }
